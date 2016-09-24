181978f424f33a9ffb02bab7c0cbbd663a2f83b1
MaximeJannot <max.jannot@gmail.com>
2016-09-24 14:23:04 +0200

    0.1.0 correction localStorage checkbox

--- script.js
+++ script.js
@@ -34,7 +34,18 @@ $(document).ready(function(){
 		}
 	}
 
-	blocAccordion(35);
+	//cleanArray removes all duplicated elements
+	function cleanArray(array) {
+	  var i, j, len = array.length, out = [], obj = {};
+	  for (i = 0; i < len; i++) {
+	    obj[array[i]] = 0;
+	  }
+	  for (j in obj) {
+	    out.push(j);
+	  }
+	  return out;
+	}
+
 
 // ETAPE 1
 	var Etape_1_id = ['souris_grise', 
@@ -60,9 +71,6 @@ $(document).ready(function(){
 						'Larve Bleue (Un peu partout)',
 						'Crabe (Zone du Port de Madrestam, Rivages de Sufokia)',
 						'Tofu (Amakna, Astrub)'];
-
-	etape_check(Etape_1_id, Etape_1_info, '1');
-
 // ETAPE 2
 	var Etape_2_id = ['tofu_malade', 
 					  'champ_champ', 
@@ -100,49 +108,20 @@ $(document).ready(function(){
 						'Bandit du clan des Roublards (Territoire des Bandits)',
 						'Gobelin ([4, -12], Campement des Bworks, Village dévasté de Gisgoul)'];
 
+
+	blocAccordion(35);
+	etape_check(Etape_1_id, Etape_1_info, '1');
 	etape_check(Etape_2_id, Etape_2_info, '2');
 
 
-
-/*	function createCookie(name,value,days) {
-		if (days) {
-			var date = new Date();
-			date.setTime(date.getTime()+(days*24*60*60*1000));
-			var expires = "; expires="+date.toGMTString();
-		}
-
-		else var expires = "";
-		document.cookie = name+"="+value+expires+"; path=/";
-	}
-
-	function readCookie(name) {
-		var nameEQ = name + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
-			var c = ca[i];
-			while (c.charAt(0)==' ') c = c.substring(1,c.length);
-			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-		}
-		return null;
-	}
-
-	function eraseCookie(name) {
-		createCookie(name,"",-1);
-	}*/
-
-	//cleanArray removes all duplicated elements
-	function cleanArray(array) {
-	  var i, j, len = array.length, out = [], obj = {};
-	  for (i = 0; i < len; i++) {
-	    obj[array[i]] = 0;
-	  }
-	  for (j in obj) {
-	    out.push(j);
-	  }
-	  return out;
-	}
-
 	var checkedArray = [];
+	if (localStorage.getItem('checkbox') != null) {
+		var valCheck = localStorage.getItem('checkbox').split(",");
+		for (var i = 0, c = valCheck.length; i < c; i++) {
+			checkedArray.push(valCheck[i]);
+			jQuery("#"+valCheck[i]).prop("checked", true);
+		}		
+	}
 	jQuery('input[type="checkbox"]').click(function() {
 		var valueCheck = jQuery(this).attr('id');
 		if (jQuery(this).prop("checked") == true) {
@@ -156,17 +135,8 @@ $(document).ready(function(){
 			    }
 			}
 			localStorage.setItem('checkbox', checkedArray)
-			console.log(checkedArray);
 		}
 	});
-
-	if (localStorage.getItem('checkbox') != null) {
-		var valCheck = localStorage.getItem('checkbox').split(",");
-
-		for (var i = 0, c = valCheck.length; i < c; i++) {
-			jQuery("#"+valCheck[i]).prop("checked", true);
-		}		
-	}
 
 });
 
