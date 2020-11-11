89f73372ea85a7de4af545049b0053d737b73b55
Yohann Bethoule <yoyoteva@gmail.com>
2020-11-11 20:56:04 +0100

    MAJ systeme sauvegarde/chargement des données

--- creataccordion.js
+++ creataccordion.js
@@ -1,32 +1,4 @@
-/*
- * Créer autant de bloc accordion qu'il y a d'étape
- */
-for (var i = 1, c = 36; i < c; i++) {
-	$('.js-accordion').append(
-		'<h2 class="js-accordion__header">Etape '+i+'</h2>'+
-		'<div class="js-accordion__panel">'+
-			'<input type="checkbox" data-check="etape" data-id="'+i+'" id="check_all_etape_'+i+'" name="check_all_etape_'+i+'"/>'+
-			'<label for="check_all_etape_'+i+'">Tout cocher</label>'+
-			'<div id="barre_etape_'+i+'" aria-hidden="true"></div>'+
-			'<ul id="etape_'+i+'">'+
-			'</ul>' +
-		'</div>'
-	)
-}
 
-$.getJSON('etapes.JSON',function(data){
-	$.each( data.etape, function( key, value ) {
-	  	for (var i = 0, c = value.length; i < c; i++) {
-	  		$('#'+key).append(
-		    	'<li>'
-		    		+'<img class="img-bestiaire" src="'+ value[i].imgBestiaire +'" alt>'
-		    		+'<input type="checkbox" data-check="monstre" data-etape="'+key+'" name="'+key+'_id'+i+'" id="'+key+'_id'+i+'" />'
-	    	 		+'<label for="'+key+'_id'+i+'"><span class="checkMonstre">'+value[i].monstre+'</span> <span class="checkLieu">'+value[i].lieu+'</span></label>'+
-    	 		'</li>'
-	 		);
-	  	}
-	});
-});
 
 
 
--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
 
 	/*
 	 * Cache/Affiche les etapes validées
@@ -80,85 +80,130 @@ $(document).ready(function(){
 		}
 	}
 
+	function check_monstre(self){
+		var valueCheck =self.attr('id');
+		var etape = self.data('etape');
+		/* Si la checkbox est activé > ajoute la valeur */
+		/* Sinon supprime la valeur du checkbox cliqué et réinitialise le localstorage */
 
-	/*
-	 *
-	 * Ces fonctions permettre de garder en mémoire
-	 * dans LocalStorage les checkbox selectionnés
-	 *
-	 */
-	/* Vérifie au chargement de la page si le visiteur à deja activé des checkbox */
-	var checkedArray = [];
-	if (localStorage.getItem('checkbox') != null) {
-		var valCheck = localStorage.getItem('checkbox').split(",");
-		for (var i = 0, c = valCheck.length; i < c; i++) {
-			if(valCheck[i]){
-				checkedArray.push(valCheck[i]);
-				$("#" + valCheck[i]).prop("checked", true);
+		if (self.prop("checked") == true) {
+			checkedArray.push(valueCheck);
+			checkedArray = cleanArray(checkedArray);
+			/* On toggle le mode cocher/décocher du checkbox étape si c'est le dernier monstre validé de la liste */
+			if($('input[type="checkbox"]').filter('[data-etape="'+etape+'"]').filter(':checked').length
+				== $('input[type="checkbox"]').filter('[data-etape="'+etape+'"]').length){
+				$('input[data-id="'+etape.split('_')[1]+'"]').prop("checked", true)
+				toggle_cocher_dédocher(etape.split('_')[1])
+			}
+		} else {
+			$('input[data-id="'+etape.split('_')[1]+'"]').prop("checked", false)
+			toggle_cocher_dédocher(etape.split('_')[1])
+
+			for(var i = checkedArray.length - 1; i >= 0; i--) {
+				if(checkedArray[i] === valueCheck) {
+					checkedArray.splice(i, 1);
+				}
 			}
 		}
 	}
 
 
+	var checkedArray = [];
 
-	/* Ajoute le checkbox checké dans le LocalStorage */
-	$('input[type="checkbox"]').change(function() {
-		console.log('blip')
-		var self = $(this);
-		/* Si la checkbox est la checkbox d'un monstre*/
-		if(self.data('check') == "monstre"){
-			var valueCheck =self.attr('id');
-			var etape = self.data('etape');
-			/* Si la checkbox est activé > ajoute la valeur */
-			/* Sinon supprime la valeur du checkbox cliqué et réinitialise le localstorage */
-			if (self.prop("checked") == true) {
-				checkedArray.push(valueCheck);
-				checkedArray = cleanArray(checkedArray);
-				/* On toggle le mode cocher/décocher du checkbox étape si c'est le dernier monstre validé de la liste */
-				console.log($('input[type="checkbox"]').filter('[data-etape="'+etape+'"]').filter(':checked').length)
-				console.log($('input[type="checkbox"]').filter('[data-etape="'+etape+'"]').length)
-				if($('input[type="checkbox"]').filter('[data-etape="'+etape+'"]').filter(':checked').length
-						== $('input[type="checkbox"]').filter('[data-etape="'+etape+'"]').length){
-					console.log($('input[data-id="'+etape.split('_')[1]+'"]'))
-					$('input[data-id="'+etape.split('_')[1]+'"]').prop("checked", true)
-					toggle_cocher_dédocher(etape.split('_')[1])
+
+
+
+
+	/* Chargement des données */
+	function loadData(){
+		/* Ajoute le checkbox checké dans le LocalStorage */
+		$('input[type="checkbox"]').change(function() {
+			var self = $(this);
+			/* Si la checkbox est la checkbox d'un monstre*/
+			if(self.data('check') == "monstre"){
+				check_monstre(self)
+				/* Si on check une étape complète*/
+			}else if(self.data('check') == "etape"){
+				let etape_ip = self.data("id");
+				if(self.is(':checked')){
+					/* On coche toutes les checkbox de l'étape */
+					$('input[type="checkbox"]').filter('[data-etape="etape_'+etape_ip+'"]').prop("checked", true).each(function () {
+						check_monstre($(this))
+					});
+				}else{
+					/* On décoche toutes les checkbox de l'étape */
+					$('input[type="checkbox"]').filter('[data-etape="etape_'+etape_ip+'"]').prop("checked", false).each(function () {
+						check_monstre($(this))
+					});
 				}
-			} else {
-				if($('input[type="checkbox"]').filter('[data-etape="'+etape+'"]').filter(':checked').length
-					!= $('input[type="checkbox"]').filter('[data-etape="'+etape+'"]').length){
-					console.log('decocher')
-					$('input[data-id="'+etape.split('_')[1]+'"]').prop("checked", false)
-					toggle_cocher_dédocher(etape.split('_')[1])
-				}
-				for(var i = checkedArray.length - 1; i >= 0; i--) {
-					if(checkedArray[i] === valueCheck) {
-						checkedArray.splice(i, 1);
-					}
+				toggle_cocher_dédocher(etape_ip)
+			}
+
+			localStorage.setItem('checkbox', checkedArray);
+
+			progressBarEtape();
+			progressBarAllEtape();
+		});
+
+		/* Vérifie au chargement de la page si le visiteur à deja activé des checkbox */
+		if (localStorage.getItem('checkbox') != null) {
+			var valCheck = localStorage.getItem('checkbox').split(",");
+			for (var i = 0, c = valCheck.length; i < c; i++) {
+				if(valCheck[i]){
+					checkedArray.push(valCheck[i]);
+					$("#" + valCheck[i]).prop("checked", true).change();
 				}
 			}
-		/* Si on check une étape complète*/
-		}else if(self.data('check') == "etape"){
-			let etape_ip = self.data("id");
-			if(self.is(':checked')){
-				/* On coche toutes les checkbox de l'étape */
-				$('input[type="checkbox"]').filter('[data-etape="etape_'+etape_ip+'"]').prop("checked", true);
-			}else{
-				/* On décoche toutes les checkbox de l'étape */
-				$('input[type="checkbox"]').filter('[data-etape="etape_'+etape_ip+'"]').prop("checked", false);
-			}
-			toggle_cocher_dédocher(etape_ip)
 		}
 
-		localStorage.setItem('checkbox', checkedArray);
-
-		/* Calcule la valeur des progress bar */
 		progressBarEtape();
 		progressBarAllEtape();
-	});
-
-	progressBarEtape();
-	progressBarAllEtape();
-	for (i=1; i<=35; i++){
-		toggle_cocher_dédocher(i)
+		for (i=1; i<=35; i++){
+			toggle_cocher_dédocher(i)
+		}
 	}
+
+	$('html').css('cursor', 'wait');
+	$('#loading_data').css('visibility', 'visible');
+
+	/*
+	* Créer autant de bloc accordion qu'il y a d'étape
+ 	*/
+	for (var i = 1, c = 36; i < c; i++) {
+		$('.js-accordion').append(
+			'<h2 class="js-accordion__header">Etape '+i+'</h2>'+
+			'<div class="js-accordion__panel">'+
+			'<input type="checkbox" data-check="etape" data-id="'+i+'" id="check_all_etape_'+i+'" name="check_all_etape_'+i+'"/>'+
+			'<label for="check_all_etape_'+i+'">Tout cocher</label>'+
+			'<div id="barre_etape_'+i+'" aria-hidden="true"></div>'+
+			'<ul id="etape_'+i+'">'+
+			'</ul>' +
+			'</div>'
+		)
+	}
+
+	$.getJSON('etapes.JSON',function(data){
+		$.each( data.etape, function( key, value ) {
+			for (var i = 0, c = value.length; i < c; i++) {
+				$('#'+key).append(
+					'<li>'
+					+'<img class="img-bestiaire" src="'+ value[i].imgBestiaire +'" alt>'
+					+'<input type="checkbox" data-check="monstre" data-etape="'+key+'" name="'+key+'_id'+i+'" id="'+key+'_id'+i+'" />'
+					+'<label for="'+key+'_id'+i+'"><span class="checkMonstre">'+value[i].monstre+'</span> <span class="checkLieu">'+value[i].lieu+'</span></label>'+
+					'</li>'
+				);
+			}
+		});
+		loadData();
+		$.getScript("bower_components/jquery-accessible-accordion-aria/jquery-accessible-accordion-aria-legacy-1.2.0.js", function () {
+			/* Calcule la valeur des progress bar */
+			setTimeout(function(){
+				progressBarEtape();
+				progressBarAllEtape();
+
+				$('#loading_data').css('visibility', 'hidden');
+				$('html').css("cursor", "default");
+			}, 500);
+		});
+	});
 });
